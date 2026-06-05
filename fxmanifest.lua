01132d2d005adbfa1d99bdaa96a4c2a3146d0037
lone <icyscripts5m@gmail.com>
2026-06-05 09:14:18 +0300

    Update author in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 
 game 'gta5'
 
-author 'valentino'
+author 'lone/Ragdoll Development'
 version '1.0.0'
 description 'Configurable nametags with /tognames, /mark/friend, /nmask; ESX/QBCore/QBX compatible; ox_lib notifications.'
 
